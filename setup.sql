eefded85ce64993a7f3e6e32e35a0bf590207ac4
JeffAllison <itsjallison@gmail.com>
2022-08-27 17:27:15 -0700

    added table columns to posts table

--- setup.sql
+++ setup.sql
@@ -2,14 +2,15 @@ DROP TABLE IF EXISTS posts CASCADE;
 
 CREATE TABLE posts (
 	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	caption VARCHAR NOT NULL,
-	image_url VARCHAR(128) NOT NULL,
-	cloudinary_id VARCHAR(128) NOT NULL
+	caption VARCHAR,
+	image_url VARCHAR(128) NOT NULL
+	-- cloudinary_id VARCHAR(128) NOT NULL
 );
 
 INSERT INTO posts (
-caption
+caption,
+image_url
 )
 VALUES (
-'is it trash?'
+'is it trash?', ''
 );
