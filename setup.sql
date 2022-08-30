baec429a2233e05dbc27531f71aa3c179b4eaef8
JeffAllison <itsjallison@gmail.com>
2022-08-30 11:51:46 -0700

    added sql table for comments, added comment controller file and model file for getting comments by post ID and posting a comment to a trash post

--- setup.sql
+++ setup.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS posts CASCADE;
 DROP TABLE IF EXISTS app_users CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
 
 CREATE TABLE posts (
 	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -15,11 +16,31 @@ CREATE TABLE app_users (
 	username TEXT
 );
 
+CREATE TABLE comments (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  --post_ID make not nulll!!!!
+  post_id INT,
+  text TEXT,
+  timestamp TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  --user make not nulll!!!!
+  user_id INT 
+  -- FOREIGN KEY (user_id) REFERENCES app_users(id),
+  -- FOREIGN KEY (post_id) REFERENCES posts(id)
+);
+
 INSERT INTO posts (
 caption,
 image_url
 )
+
 VALUES 
 ('is it trash?', ''),
 ('this is another caption', ''),
 ('and another caption', '');
+
+INSERT INTO comments (
+text
+)
+
+VALUES 
+('');
